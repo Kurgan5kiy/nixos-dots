0e37f2204313f4e95b308403ad47f91bb601510d
beholder <kurganskiyvladislav@gmail.com>
2026-04-22 01:46:37 +0200

    Force gtk

--- system/xdg-portal.nix
+++ system/xdg-portal.nix
@@ -1,14 +1,14 @@
-{ pkgs, ... }: {
+{ pkgs, lib, ... }: {
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
     config = {
       common.default = [ "gtk" ];
       niri = {
-        default = [ "gtk" ];
-        "org.freedesktop.impl.portal.Screencast" = [ "gnome" ];
-        "org.freedesktop.impl.portal.Screenshot" = [ "gnome" ];
-        "org.freedesktop.impl.portal.Secret" = [ "gnome-keyring" ];
+        default = lib.mkForce [ "gtk" ];
+        "org.freedesktop.impl.portal.Screencast" = lib.mkForce [ "gnome" ];
+        "org.freedesktop.impl.portal.Screenshot" = lib.mkForce [ "gnome" ];
+        "org.freedesktop.impl.portal.Secret" = lib.mkForce [ "gnome-keyring" ];
       };
     };
   };
