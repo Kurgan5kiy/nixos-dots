23d09403088b6b41fe734bcdc0f1470c798f54bf
beholder <kurganskiyvladislav@gmail.com>
2026-04-24 00:56:53 +0200

    Create placeholder/template stylix files.

---
+++ home/stylix.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }:
+{
+  stylix = {
+    # enable = true;
+    # base16Scheme = ...
+    # image = ...
+    # fonts = { ... };
+  };
+}
---
+++ system/stylix.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }:
+{
+  stylix = {
+    # enable = true;
+    # base16Scheme = ...
+    # image = ...
+  };
+}
