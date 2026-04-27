508f2d3e74d9e6bbd5e6fc8f721d58c4fa600aa2
beholder <kurganskiyvladislav@gmail.com>
2026-04-27 02:00:44 +0200

    Add dev-shells support.

--- flake.nix
+++ flake.nix
@@ -17,13 +17,16 @@
       url = "github:danth/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-python, ... }@inputs:
     let
       lib = nixpkgs.lib;
       vars = import ./variables.nix;
       theme = import ./theme.nix;
+      pkgs = nixpkgs.legacyPackages.${vars.system};
+      py3143 = nixpkgs-python.packages.${vars.system}."3.14.3"; 
     in {
     nixosConfigurations = {
       ${vars.hostName} = lib.nixosSystem {
@@ -31,10 +34,31 @@
         specialArgs = { inherit self inputs vars theme; };
         modules = [
           ./system
-          ./home # import modular home setup
+          ./home
           inputs.stylix.nixosModules.stylix
           inputs.home-manager.nixosModules.home-manager
         ];
+      };
+    };
+
+    devShells.${vars.system} = {
+      default = pkgs.mkShell {
+        packages = [
+          py3143
+          pkgs.poetry
+          pkgs.uv
+        ];
+
+        env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+          pkgs.stdenv.cc.cc.lib
+          pkgs.zlib
+        ];
+
+        shellHook = ''
+          export POETRY_VIRTUALENVS_IN_PROJECT=true
+          export UV_PROJECT_ENVIRONMENT=$PWD/.venv
+          echo "Using exact Python version: $(python --version)"
+        '';
       };
     };
   };
--- home/nvim.nix
+++ home/nvim.nix
@@ -3,7 +3,7 @@
   home.sessionVariables.EDITOR = "nvim";
 
   home.packages = with pkgs; [
-    inputs.neovim-nightly-overlay.packages.${pkgs.system}.default
+    inputs.neovim-nightly-overlay.packages.${pkgs.stdenv.hostPlatform.system}.default
     nodejs
     python3
     python3Packages.pip
